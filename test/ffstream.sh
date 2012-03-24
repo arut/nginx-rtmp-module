fa6d9fc84f29032b26ebc1e5713cdbbed1a27344
Roman Arutyunyan <arut@qip.ru>
2012-03-24 10:26:07 +0400

    README

--- test/ffstream.sh
+++ test/ffstream.sh
@@ -1,2 +1,2 @@
-ffmpeg -loglevel verbose -re -i /mnt/home/rarutyunyan/Videos/anything_else-solaris.giga.su.avi  -f flv rtmp://localhost/helo
+ffmpeg -loglevel verbose -re -i /mnt/home/rarutyunyan/Videos/anything_else-solaris.giga.su.avi  -f flv rtmp://localhost/helo/movie
 #ffmpeg -loglevel verbose -re -i /mnt/home/rarutyunyan/Videos/the_changeup-solaris.giga.su.avi -an -f flv rtmp://localhost/helo
