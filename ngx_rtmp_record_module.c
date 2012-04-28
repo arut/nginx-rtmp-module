239b947b4dfff112a72a0e6f0bfcda8a0cf92654
Roman Arutyunyan <arut@qip.ru>
2012-04-28 20:43:58 +0400

    fixed delete_stream call chain breakup

--- ngx_rtmp_record_module.c
+++ ngx_rtmp_record_module.c
@@ -411,9 +411,7 @@ static ngx_int_t
 ngx_rtmp_record_delete_stream(ngx_rtmp_session_t *s, 
         ngx_rtmp_delete_stream_t *v)
 {
-    if (ngx_rtmp_record_close(s) != NGX_OK) {
-        return NGX_ERROR;
-    }
+    ngx_rtmp_record_close(s);
 
     return next_delete_stream(s, v);
 }
