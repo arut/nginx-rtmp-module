c9973fc68a91284c632f3afb736303d6402f6c4e
Roman Arutyunyan <arut@qip.ru>
2012-06-27 21:20:41 +0400

    added per-relay parameters; app, pageUrl, swfUrl etc

--- ngx_rtmp_relay_module.h
+++ ngx_rtmp_relay_module.h
@@ -10,10 +10,25 @@
 #include "ngx_rtmp.h"
 
 
-ngx_int_t ngx_rtmp_relay_pull(ngx_rtmp_session_t *s, ngx_str_t *app, 
-                              ngx_str_t *name, ngx_url_t *url);
-ngx_int_t ngx_rtmp_relay_push(ngx_rtmp_session_t *s, ngx_str_t *app,
-                              ngx_str_t *name, ngx_url_t *url);
+typedef struct {
+    ngx_url_t                       url;
+    ngx_str_t                       app;
+    ngx_str_t                       name;
+    ngx_str_t                       tc_url;
+    ngx_str_t                       page_url;
+    ngx_str_t                       swf_url;
+    ngx_str_t                       flash_ver;
+    ngx_str_t                       play_path;
+    ngx_int_t                       live;
+    ngx_int_t                       start;
+    ngx_int_t                       stop;
+} ngx_rtmp_relay_target_t;
+
+
+ngx_int_t ngx_rtmp_relay_pull(ngx_rtmp_session_t *s, ngx_str_t *name,
+                              ngx_rtmp_relay_target_t *target);
+ngx_int_t ngx_rtmp_relay_push(ngx_rtmp_session_t *s, ngx_str_t *name,
+                              ngx_rtmp_relay_target_t *target);
 
 
 #endif /* _NGX_RTMP_RELAY_H_INCLUDED_ */
