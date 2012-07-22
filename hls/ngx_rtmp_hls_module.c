5070ff151a7605a3a9a065496f6a87b3a4eabb5b
Roman Arutyunyan <arut@qip.ru>
2012-07-22 09:48:25 +0400

    disabled HLS for auto-pushed connection

--- hls/ngx_rtmp_hls_module.c
+++ hls/ngx_rtmp_hls_module.c
@@ -741,6 +741,11 @@ ngx_rtmp_hls_publish(ngx_rtmp_session_t *s, ngx_rtmp_publish_t *v)
     if (hacf == NULL || !hacf->hls || hacf->path.len == 0) {
         goto next;
     }
+
+    if (s->auto_pushed) {
+        goto next;
+    }
+
     ngx_log_debug2(NGX_LOG_DEBUG_RTMP, s->connection->log, 0,
             "hls: publish: name='%s' type='%s'",
             v->name, v->type);
