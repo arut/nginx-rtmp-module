5c511bd4f029d421c315fe514932f7e70185c1f2
Neutron Soutmun <neo.neutron@gmail.com>
2012-08-09 02:11:38 +0000

    Fixed the netcall arguments are not escaped properly
    The netcall arguments should be escaped with NGX_ESCAPE_ARGS flag.

--- ngx_rtmp_netcall_module.c
+++ ngx_rtmp_netcall_module.c
@@ -577,22 +577,22 @@ ngx_rtmp_netcall_http_format_session(ngx_rtmp_session_t *s, ngx_pool_t *pool)
     b->last = ngx_cpymem(b->last, (u_char*)"&flashver=", 
             sizeof("&flashver=") - 1);
     b->last = (u_char*)ngx_escape_uri(b->last, s->flashver.data, 
-            s->flashver.len, 0);
+            s->flashver.len, NGX_ESCAPE_ARGS);
 
     b->last = ngx_cpymem(b->last, (u_char*)"&swfurl=", 
             sizeof("&swfurl=") - 1);
     b->last = (u_char*)ngx_escape_uri(b->last, s->swf_url.data, 
-            s->swf_url.len, 0);
+            s->swf_url.len, NGX_ESCAPE_ARGS);
 
     b->last = ngx_cpymem(b->last, (u_char*)"&tcurl=", 
             sizeof("&tcurl=") - 1);
     b->last = (u_char*)ngx_escape_uri(b->last, s->tc_url.data, 
-            s->tc_url.len, 0);
+            s->tc_url.len, NGX_ESCAPE_ARGS);
 
     b->last = ngx_cpymem(b->last, (u_char*)"&pageurl=", 
             sizeof("&pageurl=") - 1);
     b->last = (u_char*)ngx_escape_uri(b->last, s->page_url.data, 
-            s->page_url.len, 0);
+            s->page_url.len, NGX_ESCAPE_ARGS);
 
     return cl;
 }
