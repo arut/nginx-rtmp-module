6c79085d5c4203d11d9aa53d456ef7195de8cdc4
Roman Arutyunyan <arut@qip.ru>
2013-01-10 00:03:08 +0400

    fixed format

--- ngx_rtmp_stat_module.c
+++ ngx_rtmp_stat_module.c
@@ -354,7 +354,7 @@ ngx_rtmp_stat_live(ngx_http_request_t *r, ngx_chain_t ***lll,
                     NGX_RTMP_STAT_L("</time>");
 
                     NGX_RTMP_STAT_L("<dropped>");
-                    NGX_RTMP_STAT(buf, ngx_snprintf(buf, sizeof(buf), "%%uD",
+                    NGX_RTMP_STAT(buf, ngx_snprintf(buf, sizeof(buf), "%uD",
                                                     ctx->ndropped)
                                        - buf);
                     NGX_RTMP_STAT_L("</dropped>");
