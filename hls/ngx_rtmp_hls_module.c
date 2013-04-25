b0de943956c4c11e90874b0a643eac3c1e28fc96
Roman Arutyunyan <arut@qip.ru>
2013-04-25 20:20:59 +0400

    style fix

--- hls/ngx_rtmp_hls_module.c
+++ hls/ngx_rtmp_hls_module.c
@@ -863,8 +863,8 @@ ngx_rtmp_hls_restore_stream(ngx_rtmp_session_t *s)
                 ngx_log_debug6(NGX_LOG_DEBUG_RTMP, s->connection->log, 0,
                                "hls: restore fragment '%*s' id=%uL, "
                                "duration=%.3f, frag=%uL, nfrags=%ui",
-                               (size_t) (last - p), p, f->id, f->duration, ctx->frag,
-                               ctx->nfrags);
+                               (size_t) (last - p), p, f->id, f->duration,
+                               ctx->frag, ctx->nfrags);
             }
 
             p = next;
