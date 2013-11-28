d01ffc0c88d9ce736be3dee99b4f9d3fcc07b685
Roman Arutyunyan <arut@qip.ru>
2013-11-28 22:19:36 +0400

    increased version

--- ngx_rtmp_version.h
+++ ngx_rtmp_version.h
@@ -8,8 +8,8 @@
 #define _NGX_RTMP_VERSION_H_INCLUDED_
 
 
-#define nginx_rtmp_version  1000007
-#define NGINX_RTMP_VERSION  "1.0.7"
+#define nginx_rtmp_version  1000008
+#define NGINX_RTMP_VERSION  "1.0.8"
 
 
 #endif /* _NGX_RTMP_VERSION_H_INCLUDED_ */
