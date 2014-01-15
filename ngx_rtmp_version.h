036a7fd1d5212727819e35dbd781752ac77b0d44
Roman Arutyunyan <arut@qip.ru>
2014-01-15 21:06:01 +0400

    version up to 1.1.2

--- ngx_rtmp_version.h
+++ ngx_rtmp_version.h
@@ -8,8 +8,8 @@
 #define _NGX_RTMP_VERSION_H_INCLUDED_
 
 
-#define nginx_rtmp_version  1001001
-#define NGINX_RTMP_VERSION  "1.1.1"
+#define nginx_rtmp_version  1001002
+#define NGINX_RTMP_VERSION  "1.1.2"
 
 
 #endif /* _NGX_RTMP_VERSION_H_INCLUDED_ */
