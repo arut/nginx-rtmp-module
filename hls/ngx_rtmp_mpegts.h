51ab327abfa9f328a85646a3f4364c810277e47f
Roman Arutyunyan <arut@qip.ru>
2014-08-31 18:58:43 +0400

    fixed style

--- hls/ngx_rtmp_mpegts.h
+++ hls/ngx_rtmp_mpegts.h
@@ -39,7 +39,7 @@ ngx_int_t ngx_rtmp_mpegts_open_file(ngx_rtmp_mpegts_file_t *file, u_char *path,
     ngx_log_t *log);
 ngx_int_t ngx_rtmp_mpegts_close_file(ngx_rtmp_mpegts_file_t *file);
 ngx_int_t ngx_rtmp_mpegts_write_frame(ngx_rtmp_mpegts_file_t *file,
-      ngx_rtmp_mpegts_frame_t *f, ngx_buf_t *b);
+    ngx_rtmp_mpegts_frame_t *f, ngx_buf_t *b);
 
 
 #endif /* _NGX_RTMP_MPEGTS_H_INCLUDED_ */
