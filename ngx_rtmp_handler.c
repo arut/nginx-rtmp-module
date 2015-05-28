a4907e80ce33148e0bc8da35d8685376315977f6
Eugaulerss <Eugaulerss@users.noreply.github.com>
2015-05-28 16:47:23 +0800

    Update ngx_rtmp_handler.c
    fixbug lost data when chunk_size changing, the frontier of stream data should by pointed by the pos and last of ngx_buf_t

--- ngx_rtmp_handler.c
+++ ngx_rtmp_handler.c
@@ -868,6 +868,7 @@ ngx_rtmp_set_chunk_size(ngx_rtmp_session_t *s, ngx_uint_t size)
 
                 bi->pos += (ngx_cpymem(bo->last, bi->pos,
                             bo->end - bo->last) - bo->last);
+                bo->last = bo->end;
                 lo->next = ngx_rtmp_alloc_in_buf(s);
                 lo = lo->next;
                 if (lo == NULL) {
