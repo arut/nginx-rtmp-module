28f0acce2ebcd20f236e2d804a2ebef77b0f5640
Andrei Belov <defanator@gmail.com>
2018-12-22 02:39:31 +0300

    Fixed building with gcc 8

--- ngx_rtmp_eval.c
+++ ngx_rtmp_eval.c
@@ -166,6 +166,7 @@ ngx_rtmp_eval(void *ctx, ngx_str_t *in, ngx_rtmp_eval_t **e, ngx_str_t *out,
                         state = ESCAPE;
                         continue;
                 }
+                /* fall through */
 
             case ESCAPE:
                 ngx_rtmp_eval_append(&b, &c, 1, log);
