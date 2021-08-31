0eb48baeaa2a80216c790955c91e9a427f9fa306
CrNMGuy <florian.duval@gmail.com>
2021-09-01 00:52:51 +0200

    Version update

--- ngx_rtmp_version.h
+++ ngx_rtmp_version.h
@@ -8,8 +8,8 @@
 #define _NGX_RTMP_VERSION_H_INCLUDED_
 
 
-#define nginx_rtmp_version  1001004
-#define NGINX_RTMP_VERSION  "1.1.4"
+#define nginx_rtmp_version  1002002
+#define NGINX_RTMP_VERSION  "1.2.2"
 
 
 #endif /* _NGX_RTMP_VERSION_H_INCLUDED_ */
